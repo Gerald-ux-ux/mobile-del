abb72f0510782b8f246e7d3bc8be8f4097bb0dad
Gerald-ux-ux <11geraldmoney@gmail.com>
2023-02-06 21:17:49 +0300

    building the home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { View, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
 
 const HomeScreen = () => {
+  
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+
+    })
+
+
+  },[])
+
+
   return (
-    <View>
-      <Text style={{ color: "red" }}> Home Screen</Text>
-    </View>
+    <SafeAreaView>
+      <Text style={{ color: "red-500" }}> Home Screen</Text>
+    </SafeAreaView>
   );
 };
 
